42263fd502c600823c75ef66e91e92d84d552f74
nclima <nadia.clima@gmail.com>
2020-05-30 15:51:19 +0300

    added exception handling

--- src/theory/oopconcepts/polymorphism/methodoverriding/OverridingChildClass.java
+++ src/theory/oopconcepts/polymorphism/methodoverriding/OverridingChildClass.java
@@ -1,5 +1,11 @@
 package theory.oopconcepts.polymorphism.methodoverriding;
 
+/***
+ * RULES:
+ * 1. Arguments List: Arguments list in the overriding method should be the same as in the parent class overridden method
+ * 2. Access Modifier: The access modifier of the overriding method cannot be more restrictive than the overridden method
+ * 3. private, static and final methods cannot be overridden as they are local to the class
+ */
 public class OverridingChildClass extends OverridingParentClass{
     public static void main(String[] args) {
         OverridingChildClass c1 = new OverridingChildClass();
